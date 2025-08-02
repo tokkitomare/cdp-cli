f0985b8d1fe4e5af9a52d763852c37f54d9b37ce
Vitor <jvjv10112607@gmail.com>
2025-08-02 15:57:06 -0300

    feat: add error handling to create_cdputils function and use the crate dirs

--- src/utils/create_cdputils.rs
+++ src/utils/create_cdputils.rs
@@ -1,12 +1,15 @@
-pub fn create_cdputils() -> Result<String, Box<dyn std::error::Error>> {
-    let home = if cfg!(windows) {
-        std::env::var("USERPROFILE")?
-    } else {
-        std::env::var("HOME")?
-    };
-    let dir = [home.as_str(), ".cdputils"].iter().collect::<std::path::PathBuf>();
+use crate::handlers::errors::{CliErr, ErrKind};
+
+pub fn create_cdputils() -> Result<String, CliErr> {
+    let home = dirs::home_dir()
+        .ok_or_else(|| CliErr::set_err("No home dir", ErrKind::DirMissing))?;
+
+    let dir = [home.to_str().unwrap(), ".cdputils"].iter().collect::<std::path::PathBuf>();
     
-    std::fs::create_dir_all(&dir)?;
+    std::fs::create_dir_all(&dir)
+        .map_err(|e| {
+            CliErr::set_err(&e.to_string(), ErrKind::IoError)
+        })?;
 
     Ok(dir.to_str().unwrap().to_string())
 }

54ad9f020f0b219d7584476d63bf7e44695e9e51
Vitor <jvjv10112607@gmail.com>
2025-08-02 15:56:24 -0300

    feat: add error handling to ls function

--- src/handlers/general/ls.rs
+++ src/handlers/general/ls.rs
@@ -1,6 +1,16 @@
-pub fn ls(path: String) {
-    let dir = std::fs::read_dir(path).unwrap();
+use crossterm::style::Stylize;
+
+use crate::handlers::errors::{CliErr, ErrKind};
+
+pub fn ls(path: String) -> Result<(), CliErr> {
+    let dir = std::fs::read_dir(path)
+        .map_err(|e| {
+            CliErr::set_err(&e.to_string(), ErrKind::DirMissing)
+        })?;
+
     for content in dir {
-        println!("{}", content.unwrap().path().display());
+        println!("-- {}", content.unwrap().path().display().to_string().replace("\\", "/").green());
     }
+
+    Ok(())
 }

b53367b458f834b051e9c3ef61a5e4cd4252e6fb
Vitor <jvjv10112607@gmail.com>
2025-08-02 15:47:45 -0300

    feat: deprecate some flags and implement best handler for errors

--- src/lib.rs
+++ src/lib.rs
@@ -3,51 +3,85 @@ mod types;
 mod utils;
 
 use clap::CommandFactory;
+use crossterm::style::Stylize;
 use types::*;
-#[allow(unused_imports)]
 use handlers::{
     general::*, aliases::*, create_project::*,
 };
 
-pub fn cli_run() -> Result<(), Box<dyn std::error::Error>> {
+use crate::{handlers::errors::CliErr, utils::parse_path};
+
+pub fn cli_run() -> Result<(), CliErr> {
     let args = Cli::parse();
-    let mut path: String;
-    if cfg!(windows) {
-        path = format!("C:\\{}", match args.command {
-            Some(Commands::General(ref cmd)) => cmd.path.clone(),
-            _ => "" .to_owned()
-        });
-    } else {
-        path = format!("{}", match args.command {
-            Some(Commands::General(ref cmd)) => cmd.path.clone(),
-            _ => "" .to_owned()
-        });
-    }
 
     match args.command {
         Some(Commands::General(cmd)) => {
+            let path = match parse_path::parse_path(cmd.path.clone()) {
+                Ok(s) => s,
+                Err(e) => {
+                    eprintln!("{e}");
+                    cmd.path
+                }
+            };
+
             if cmd.current_user {
-                path = format!("{}", change_dir_current::change_to_current());
+                let home_path = if cfg!(windows) {
+                    "%USER/"
+                } else {
+                    "~/"
+                };
+                println!(
+                    "{}: the {} (or {}) flag is no longer necessary.\nYou can specify whether the directory is in user/home using: {}your_dir", 
+                    "Deprecated".yellow(), 
+                    "--current-user".red(), 
+                    "-C".red(), 
+                    home_path.green()
+                );
             }
             if cmd.alias {
-                path = see_alias::see_alias(cmd.path.clone()).expect("This alias does not exists");
+                println!(
+                    "{}: the {} (or {}) flag is no longer necessary.\nYou can specify whether is an alias using {}. (e.g. {})", 
+                    "Deprecated".yellow(), 
+                    "--alias".red(), 
+                    "--al".red(),
+                    ":".green(),
+                    ":cdpproject/src/parse_path.rs".green()
+                );
             }
             if cmd.ls {
-                ls::ls(path.clone());
+                if let Err(e) = ls::ls(path.clone()) {
+                    eprintln!("{e}");
+                }
             }
             if cmd.vsc {
-                vsc::open_editor("vsc", path.clone());
+                println!(
+                    "{}: the {} flag is no longer necessary. Use {} (or {}) instead. (e.g. {})", 
+                    "Deprecated".yellow(), 
+                    "--vsc".red(),
+                    "--editor".green(),
+                    "-E".green(),
+                    r#"cdp general ":cdptests/assets" -E vsc"#.green()
+                );
+            }
+            if cmd.editor.is_some() {
+                if let Err(e) = editors::open_editor(Editors::Vsc, path.clone()) {
+                    eprintln!("{e}");
+                }
             }
         },
         Some(Commands::Aliases(cmd)) => {
-            aliases::aliases(cmd.path, cmd.alias)?;
+            if let Err(e) = aliases::aliases(cmd.path, cmd.alias) {
+                eprintln!("{e}");
+            }
         },
         Some(Commands::CreateProject(cmd)) => {
-            create_project::create_project(
+            if let Err(e) = create_project::create_project(
                 cmd.lang,
                 cmd.name,
                 cmd.alias,
-            )?;
+            ) {
+                eprintln!("{e}");
+            }
         },
         None => { Cli::command().print_help().unwrap(); std::process::exit(1); }
     }

bde1c1ef95a9b01f85a9f8d03e7def2f457721aa
Vitor <jvjv10112607@gmail.com>
2025-08-06 12:48:59 -0300

    refactor: improve CLI types for aliases management and project creation
    - AliasesCommand: make path and alias optional to support list, edit, remove subcommands
    - Add list, edit, remove flags for alias management
    - CPCommand: add optional path field for custom project creation location
    - Update docs and CLI argument attributes for clarity

--- src/types.rs
+++ src/types.rs
@@ -5,7 +5,7 @@ pub use clap::{Parser, Subcommand, Args};
 #[command(
     about="CDP - This CLI helps you quickly navigate to a directory and perform useful operations on it.",
     long_about="
-CDP stands for CD Program, where `cd` refers to the directory-changing command in Unix-like shells and PowerShell.
+CDP stands for CD Program, where 'cd' refers to the directory-changing command in Unix-like shells and PowerShell.
 
 This CLI helps you quickly navigate to a directory and perform useful operations on it."
 )]
@@ -16,16 +16,15 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    /// Commands to make simple operations on directories
+    /// Perform basic file and folder operations on a given path
     #[command(alias="g")]
     General(GeneralCommand),
 
-    /// Create an alias for a path so you don't have to type it out every time
+    /// Create and manage aliases for paths
     #[command(alias="alias")]
     Aliases(AliasesCommand),
 
-    /// Create a project directory with the supported langs.
-    /// It doesn't yet work on Unix
+    /// Create a project directory using a supported language
     #[command(alias="cp")]
     CreateProject(CPCommand),
 }
@@ -34,8 +33,6 @@ pub enum Commands {
 #[derive(Args, Debug)]
 pub struct GeneralCommand {
     /// Path where CDP will perform the operation
-    /// 
-    /// If no flag specifying where the program must search, it will search from the current folder.
     pub path: String,
 
     /// Lists the folders and files from the path
@@ -50,12 +47,12 @@ pub struct GeneralCommand {
     #[arg(long, short='E')]
     pub editor: Option<Editors>,
 
-    #[cfg_attr(windows, doc = "Make the command start from `USER` ($HOME)")]
-    #[cfg_attr(unix, doc = "Make the command start from `HOME` (~/)")]
+    #[cfg_attr(windows, doc = "Make the command start from 'USER' ($HOME)")]
+    #[cfg_attr(unix, doc = "Make the command start from 'HOME' (~/)")]
     #[arg(short='C', long, conflicts_with="alias", hide = true)]
     pub current_user: bool,
 
-    /// Enable the `aliases mode` and let you type path aliases instead of whole paths (to create aliases: `cdp aliases --help`)
+    /// Enable the 'aliases mode' and let you type path aliases instead of whole paths (to create aliases: 'cdp aliases --help')
     #[arg(long, alias="al", hide = true)]
     pub alias: bool,
 }
@@ -63,19 +60,39 @@ pub struct GeneralCommand {
 #[derive(Args, Debug)]
 pub struct AliasesCommand {
     /// Path that you want to create an alias
-    pub path: String,
-    /// Alias of the path
-    /// 
-    #[cfg_attr(windows, doc = "It will be stored on `cdpaliases.txt` file (file path: `%USERPROFILE%\\.cdputils\\cdpaliases.txt`)")]
-    #[cfg_attr(unix, doc = "It will be stored on `cdpaliases.txt` file (file path: `~/.cdputils/cdpaliases.txt`)")]
-    pub alias: String,
+    #[arg(value_name="PATH", required_unless_present="list", required_unless_present="edit", required_unless_present="remove")]
+    pub path: Option<String>,
+    /// Alias of the path.
+    #[cfg_attr(windows, doc = "It will be stored on 'cdpaliases.txt' file (file path: '%USERPROFILE%\\.cdputils\\cdpaliases.txt')")]
+    #[cfg_attr(unix, doc = "It will be stored on 'cdpaliases.txt' file (file path: '~/.cdputils/cdpaliases.txt')")]
+    #[arg(value_name="ALIAS", required_unless_present="list", required_unless_present="edit", required_unless_present="remove")]
+    pub alias: Option<String>,
+
+    /// Lists all the aliases in
+    #[cfg_attr(windows, doc = r"'%USERPROFILE%\.cdputils\cdpaliases.txt'")]
+    #[cfg_attr(unix, doc = "'~/.cdputils/cdpaliases.txt'")]
+    #[arg(long, short='l', alias="ls", conflicts_with="alias")]
+    pub list: bool,
+
+    /// Remove an alias from
+    #[cfg_attr(windows, doc = r"'%USERPROFILE%\.cdputils\cdpaliases.txt'")]
+    #[cfg_attr(unix, doc = "'~/.cdputils/cdpaliases.txt'")]
+    #[arg(long, short='r', alias="rm", conflicts_with="edit")]
+    pub remove: Option<String>,
+
+    /// Edit an alias from
+    #[cfg_attr(windows, doc = r"'%USERPROFILE%\.cdputils\cdpaliases.txt'")]
+    #[cfg_attr(unix, doc = "'~/.cdputils/cdpaliases.txt'")]
+    #[arg(long, short='e')]
+    pub edit: Option<String>,
 }
 
 #[derive(Args, Debug)]
 pub struct CPCommand {
-    /// Name of the project. 
-    #[cfg_attr(windows, doc = "It will be in `%USERPROFILE%/.cdputils/projects`")]
-    #[cfg_attr(unix, doc = "It will be in `~/.cdputils/projects`")]
+    /// Project name. 
+    #[cfg_attr(windows, doc = "Will be created in '%USERPROFILE%/.cdputils/projects'")]
+    #[cfg_attr(unix, doc = "Will be created in '~/.cdputils/projects'")]
+    /// by default. Use the '--path' flag to change it.
     pub name: String,
     /// Project lang
     pub lang: Langs,
@@ -83,6 +100,11 @@ pub struct CPCommand {
     /// Create an alias for the path
     #[arg(long, alias="al")]
     pub alias: Option<String>,
+    /// Create the project in a custom path (some paths require admin privileges
+    #[cfg_attr(windows, doc = r"such as 'C:\Program Files')")]
+    #[cfg_attr(unix, doc = "such as root directories (/bin, /usr, /dev, etc.))")]
+    #[arg(long, short='p')]
+    pub path: Option<String>,
 }
 
 

add40d33bbab33ff85c4bab94d0f5cfacb9bcb18
Vitor <jvjv10112607@gmail.com>
2025-11-02 12:04:21 -0300

    chore: add module exports

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,4 +1,6 @@
+pub mod setup;
 pub mod general;
 pub mod aliases;
 pub mod create_project;
+pub mod command_aliases;
 pub mod errors;
---
+++ src/utils/setup/mod.rs
@@ -0,0 +1,4 @@
+pub mod create_cdputils;
+pub mod create_cdp_cmdaliases;
+pub mod create_cdpaliases;
+pub mod create_projects;

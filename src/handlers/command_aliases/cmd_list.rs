54250179a75f0c5cb8fd3c9e93593516f89d1af2
Vitor <jvjv10112607@gmail.com>
2025-11-02 12:09:29 -0300

    feat: add commands aliases listing functionality

---
+++ src/handlers/command_aliases/cmd_list.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use crossterm::style::Stylize;
+
+use crate::{handlers::errors::{CliErr, ErrKind}, utils::setup::create_cdp_cmdaliases::create_cdp_cmdaliases};
+
+pub fn list_cmd_aliases() -> Result<(), CliErr> {
+    let path = create_cdp_cmdaliases()?;
+
+    let content = fs::read_to_string(&path)
+        .map_err(|e| {
+            CliErr::set_err(&format!("Error opening file: {e}"), ErrKind::IoError)
+        })?;
+
+    for parts in content.lines() {
+        let components = parts.split(';').collect::<Vec<_>>();
+        println!("{}: {}", components[0].green().bold(), components[1].magenta().italic().bold())
+    }
+    println!("{}: {}", "Path".yellow(), path.replace(r"\", "/").blue());
+    
+    Ok(())
+}
